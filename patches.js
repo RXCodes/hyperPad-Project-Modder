a5f31433cca0bc9fbd458f1729511d55ebea8728
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-06-23 20:42:18 -0400

    Create patches.js

---
+++ patches.js
@@ -0,0 +1,18 @@
+// initialization
+patches = {};
+console.debug("Loaded Patches");
+
+// put patches
+let zpkPatches = ["BehaviourData", "EditorData", "CollisionData", "CameraData", "LayerData", "LevelData", "ObjectData", "ObjectPosition", "PathData", "ProjectData", "TileData", "TileSelectorData"]
+zpkPatches.forEach(function(ZName) {
+  patches[ZName + " ZPK Maximum Too Low"] = false;
+});
+patches["Duplicate Scenes Found"] = false;
+patches["Ghost Objects Found"] = false;
+patches["Ghost Behaviors Found"] = false;
+
+// diagnose
+function diagnose() {
+  console.debug("Diagnosing");
+  document.getElementById("patchFixText").innerHTML = "Diagnosing Vulnerabilites...";
+}
