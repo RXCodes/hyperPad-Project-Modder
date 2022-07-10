b5a45b1bd950d743b72756f0c92ab10f903fbf72
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-07-10 06:50:35 -0400

    Update consoleLogger.js

--- code-editor/consoleLogger.js
+++ code-editor/consoleLogger.js
@@ -1,63 +1,13 @@
-    var firstOpenTerminal = true;
-        function openTerminal() {
-          if (firstOpenTerminal) {
-            logOpen = true;
-            aceCodeEditor.on("change", function(e) {
-    		  aceCodeEditor.session.removeMarker(errorLine);
-  			})
-            console.debug("Running javascript version: " + ace.version);
-            console.log("Check documentation to learn how to use this!")
-            console.log("NOTE: This is still in development, some functions will not work.")
-            logOpen = false;
-            firstOpenTerminal = false;
-          }
-          hideAlert()
-        }
-   
-        var logOpen = false;
-        
-        rewireLoggingToElement(
-    () => document.getElementById("log"),
-    () => document.getElementById("log-container"), true);
+fixLoggingFunc('log');
+fixLoggingFunc('debug');
+fixLoggingFunc('warn');
+fixLoggingFunc('error');
+fixLoggingFunc('info');
 
-function rewireLoggingToElement(eleLocator, eleOverflowLocator, autoScroll) {
-    fixLoggingFunc('log');
-    fixLoggingFunc('debug');
-    fixLoggingFunc('warn');
-    fixLoggingFunc('error');
-    fixLoggingFunc('info');
-
-    function fixLoggingFunc(name) {
-        console['old' + name] = console[name];
-        console[name] = function(...arguments) {
-            const output = produceOutput(name, arguments);
-            const eleLog = eleLocator();
-          
-          if (!logOpen) {
-            return
-          }
-
-            if (autoScroll) {
-                const eleContainerLog = eleOverflowLocator();
-                const isScrolledToBottom = eleContainerLog.scrollHeight - eleContainerLog.clientHeight <= eleContainerLog.scrollTop + 1;
-                eleLog.innerHTML += output + "<br>";
-                if (isScrolledToBottom) {
-                    eleContainerLog.scrollTop = eleContainerLog.scrollHeight - eleContainerLog.clientHeight;
-                }
-            } else {
-                eleLog.innerHTML += output + "<br>";
-            }
-
-            console['old' + name].apply(undefined, arguments);
-        };
-    }
-
-    function produceOutput(name, args) {
-        return args.reduce((output, arg) => {
-            return output +
-                "<span class=\"log-" + (typeof arg) + " log-" + name + "\">" +
-                    (typeof arg === "object" && (JSON || {}).stringify ? JSON.stringify(arg) : arg) +
-                "</span>&nbsp;";
-        }, '');
-    }
+function fixLoggingFunc(name) {
+  console['old' + name] = console[name];
+  console[name] = function(...arguments) {
+    const output = postMessage('console', name, arguments);
+    console['old' + name].apply(undefined, arguments);
+  };
 }
