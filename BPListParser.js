a21ea09197b354806cdf8a3058467260a0f4b8d7
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-07-27 00:55:42 -0400

    Update BPListParser.js

--- BPListParser.js
+++ BPListParser.js
@@ -189,7 +189,7 @@ window.extract = function(data) {
           }
           item = item.split("}")[0];
           let str = '{"UID":' + item + '}';
-          let replace = actualObj[0]["$objects"][objects[item]];
+          let replace = actualObj[0]["$objects"][item];
           console.log(str, JSON.stringify(replace));
           text = text.replaceAll(str, replace);
         });
